80a24134e4374aabe89d3c507f6bd2c85a6283c1
Gunish <gunishsharma20@gmail.com>
2023-05-24 01:28:39 -0600

    Greeting express application created

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ app.get('/',(req,res) =>{
     res.send('<h1>Hello World</h1>')
 })
 
+app.get('/greeting/:name',(req,res) => {
+    let name = req.params.name
+    const randoGreeting = ['Hello, ','Whats up, ',`Its so great to see you, `]
+    let randomNum = getRandoInt(3)
+    let message = randoGreeting[randomNum]
+
+    res.send(`<h1>${message}${name}!</h1>`)
+})
+
+function getRandoInt(range){
+    return Math.floor(Math.random()*range)
+}
+
 app.listen(port,() => {
     console.log("I'm listening on port 3000")
 })
