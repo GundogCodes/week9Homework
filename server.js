60a9edb9cf150e4218c709ca3b30b61e90df9cb2
Gunish <gunishsharma20@gmail.com>
2023-05-24 01:54:27 -0600

    Magic 8 express application created

--- server.js
+++ server.js
@@ -25,7 +25,22 @@ app.get('/tip/:total/:tipPercentage', (req,res) => {
     Your tip Percentage is: ${tipPercentage}% <br>
     Your tip Amount is: ${tipAmount}
     </h1>`)
+})
 
+app.get('/magic/:question', (req,res) => {
+    let questionAsked  = req.params.question
+    console.log('question asked',questionAsked)
+    const response = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    let randoNum = getRandoInt(20)
+    let randoResponse = response[randoNum]
+    res.send(`
+    <h1> You asked "${questionAsked}"
+    <br>
+    <br>
+    ${randoResponse}
+    </h1>
+    `)
+    //use %20 as spaces in URL
 })
 
 function getRandoInt(range){
